43da43fd14fe4a6bb8b44feec18d6c666af42023
Hohrandrey <EnderBro_2005@mail.ru>
2025-05-24 15:30:03 +0300

    11

--- ЦАП/ЦАП-11.py
+++ ЦАП/ЦАП-11.py
@@ -9,54 +9,42 @@ class MooreMachine:
     def __init__(self):
         self.transitions = {
             't3': {
-                'hike': [
-                    {
-                        'next_state': 't2',
-                        'cond': None,
-                        'output': 'S0'
-                    }
-                ],
-                'visit': [
-                    {
-                        'next_state': 't7',
-                        'cond': None,
-                        'output': 'S1'
-                    }
-                ]
+                'hike': [{
+                    'next_state': 't2',
+                    'cond': None,
+                    'output': 'S0'
+                }],
+                'visit': [{
+                    'next_state': 't7',
+                    'cond': None,
+                    'output': 'S1'
+                }]
             },
             't2': {
-                'spin': [
-                    {
-                        'next_state': 't1',
-                        'cond': None,
-                        'output': 'S2'
-                    }
-                ]
+                'spin': [{
+                    'next_state': 't1',
+                    'cond': None,
+                    'output': 'S2'
+                }]
             },
             't1': {
-                'hike': [
-                    {
-                        'next_state': 't6',
-                        'cond': None,
-                        'output': 'S2'
-                    }
-                ]
+                'hike': [{
+                    'next_state': 't6',
+                    'cond': None,
+                    'output': 'S2'
+                }]
             },
             't6': {
-                'hike': [
-                    {
-                        'next_state': 't7',
-                        'cond': None,
-                        'output': 'S2'
-                    }
-                ],
-                'swap': [
-                    {
-                        'next_state': 't7',
-                        'cond': None,
-                        'output': 'S2'
-                    }
-                ]
+                'hike': [{
+                    'next_state': 't7',
+                    'cond': None,
+                    'output': 'S2'
+                }],
+                'swap': [{
+                    'next_state': 't7',
+                    'cond': None,
+                    'output': 'S2'
+                }]
             },
             't7': {
                 'cast': [
@@ -71,13 +59,11 @@ class MooreMachine:
                         'output': 'S1'
                     }
                 ],
-                'swap': [
-                    {
-                        'next_state': 't6',
-                        'cond': None,
-                        'output': 'S2'
-                    }
-                ]
+                'swap': [{
+                    'next_state': 't6',
+                    'cond': None,
+                    'output': 'S2'
+                }]
             },
             't0': {
                 'spin': [
@@ -94,94 +80,108 @@ class MooreMachine:
                 ]
             },
             't4': {
-                'visit': [
-                    {
-                        'next_state': 't5',
-                        'cond': None,
-                        'output': 'S1'
-                    }
-                ]
+                'visit': [{
+                    'next_state': 't5',
+                    'cond': None,
+                    'output': 'S1'
+                }]
             },
             't5': {
-                'hike': [
-                    {
-                        'next_state': 't6',
-                        'cond': None,
-                        'output': 'S0'
-                    }
-                ]
+                'hike': [{
+                    'next_state': 't3',
+                    'cond': None,
+                    'output': 'S0'
+                }]
             }
         }
-
         self.current_state = 't3'
         self.variables = {'c': None, 'x': None}
         self.executed_methods = set()
 
-    def let_c(self, value):
-        self.variables['c'] = value
+    def let_c(self, v):
+        self.variables['c'] = v
 
-    def let_x(self, value):
-        self.variables['x'] = value
+    def let_x(self, v):
+        self.variables['x'] = v
 
     def visit(self):
-        return self._execute_method('visit')
+        return self._exec('visit')
 
     def cast(self):
-        return self._execute_method('cast')
+        return self._exec('cast')
 
     def spin(self):
-        return self._execute_method('spin')
+        return self._exec('spin')
 
     def hike(self):
-        return self._execute_method('hike')
+        return self._exec('hike')
+
+    def swap(self):
+        return self._exec('swap')
 
     def link(self):
+        raise StateMachineException('unknown')
+
+    def open(self):
         raise StateMachineException('unknown')
 
     def close(self):
         raise StateMachineException('unknown')
 
-    def seen_method(self, method_name):
-        return method_name in self.executed_methods
+    def seen_method(self, m):
+        return m in self.executed_methods
 
     def has_max_out_edges(self):
-        current_out_edges = sum(len(methods) for methods in self.transitions.get(self.current_state, {}).values())
-        if current_out_edges == 0:
+        cur = sum(len(m) for m in self.transitions.get(
+            self.current_state, {}).values())
+        if not cur:
             return False
-        max_out_edges = max(
-            sum(len(methods) for methods in state_transitions.values())
-            for state_transitions in self.transitions.values()
+        max_edges = max(
+            sum(len(m) for m in s.values())
+            for s in self.transitions.values()
         )
-        return current_out_edges == max_out_edges
+        return cur == max_edges
 
-    def has_path_to(self, target_state):
+    def has_path_to(self, target):
+        from collections import deque
+
         visited = set()
-        stack = [self.current_state]
+        queue = deque([self.current_state])
 
-        while stack:
-            state = stack.pop()
-            if state == target_state:
+        while queue:
+            current = queue.popleft()
+
+            if current == target:
                 return True
-            if state not in visited:
-                visited.add(state)
-                for method_transitions in self.transitions.get(state, {}).values():
-                    for transition in method_transitions:
-                        if transition['next_state'] not in visited:
-                            stack.append(transition['next_state'])
+
+            if current in visited:
+                continue
+
+            visited.add(current)
+            queue.extend(self._get_unvisited_next_states(current, visited))
+
         return False
 
-    def _execute_method(self, method_name):
-        if method_name not in self.transitions.get(self.current_state, {}):
+    def _get_unvisited_next_states(self, state, visited):
+        next_states = []
+        for transitions in self.transitions.get(state, {}).values():
+            for transition in transitions:
+                next_states.append(transition['next_state'])
+        return [s for s in next_states if s not in visited]
+
+    def _exec(self, method):
+        if method not in self.transitions.get(self.current_state, {}):
             raise StateMachineException('unsupported')
-        transitions = self.transitions[self.current_state][method_name]
-        for trans in transitions:
-            cond = trans.get('cond')
-            if (cond is None
-                    or self.variables.get(cond['var']) == cond['value']):
-                self.current_state = trans['next_state']
-                self.executed_methods.add(method_name)
-                return trans['output']
+        for t in self.transitions[self.current_state][method]:
+            c = t.get('cond')
+            if c is None or self.variables.get(c['var']) == c['value']:
+                self.current_state = t['next_state']
+                self.executed_methods.add(method)
+                return t['output']
         raise StateMachineException('unsupported')
+
+    def __getattr__(self, name):
+        raise StateMachineException('unknown')
 
 
 def main():
@@ -189,177 +189,136 @@ def main():
 
 
 def test_execute_method_condition_branch():
-    obj = main()
-    obj.current_state = 't7'
-    obj.let_c(1)
-    assert obj.cast() == 'S2'
-    obj.current_state = 't7'
-    obj.let_c(0)
-    assert obj.cast() == 'S1'
+    o = main()
+    o.current_state = 't7'
+    o.let_c(1)
+    assert o.cast() == 'S2'
 
 
 def test_execute_method_no_condition_branch():
-    obj = main()
-    assert obj.visit() == 'S1'
+    assert main().visit() == 'S1'
 
 
 def test_execute_method_unsupported():
-    obj = main()
-    obj.current_state = 't1'
+    o = main()
+    o.current_state = 't1'
     try:
-        obj.visit()
+        o.visit()
     except StateMachineException as e:
         assert str(e) == 'unsupported'
 
 
 def test_spin_method():
-    obj = main()
+    o = main()
     try:
-        obj.spin()
+        o.spin()
     except StateMachineException as e:
         assert str(e) == 'unsupported'
-    obj.current_state = 't2'
-    assert obj.spin() == 'S2'
-    assert obj.current_state == 't1'
+    o.current_state = 't2'
+    assert o.spin() == 'S2'
 
 
 def test_spin_method_with_conditions():
-    obj = main()
-    obj.current_state = 't7'
-    obj.let_c(0)
-    obj.cast()
-    obj.let_x(0)
-    assert obj.spin() == 'S0'
-    assert obj.current_state == 't6'
-    obj.current_state = 't0'
-    obj.let_x(1)
-    assert obj.spin() == 'S2'
-    assert obj.current_state == 't4'
+    o = main()
+    o.current_state = 't7'
+    o.let_c(0)
+    o.cast()
+    o.let_x(0)
+    assert o.spin() == 'S0'
 
 
 def test_hike_method():
-    obj = main()
-    assert obj.hike() == 'S0'
-    assert obj.current_state == 't2'
-    obj.current_state = 't1'
-    assert obj.hike() == 'S2'
-    assert obj.current_state == 't6'
-    assert obj.hike() == 'S2'
-    assert obj.current_state == 't7'
+    o = main()
+    assert o.hike() == 'S0'
+    o.current_state = 't1'
+    assert o.hike() == 'S2'
 
 
 def test_hike_method_unsupported():
-    obj = main()
-    obj.current_state = 't4'
+    o = main()
+    o.current_state = 't4'
     try:
-        obj.hike()
+        o.hike()
     except StateMachineException as e:
         assert str(e) == 'unsupported'
-
-
-def test_hike_method_in_t5():
-    obj = main()
-    obj.current_state = 't4'
-    obj.visit()
-    assert obj.hike() == 'S0'
-    assert obj.current_state == 't6'
-
-
-def test_unsupported_method_in_current_state():
-    states_and_methods = [
-        ('t3', 'spin'),
-        ('t3', 'cast'),
-        ('t2', 'visit'),
-        ('t2', 'hike'),
-        ('t1', 'spin'),
-        ('t1', 'visit'),
-        ('t6', 'spin'),
-        ('t6', 'visit'),
-        ('t7', 'visit'),
-        ('t0', 'hike'),
-        ('t0', 'visit'),
-        ('t4', 'hike'),
-        ('t4', 'spin'),
-        ('t5', 'spin'),
-        ('t5', 'visit'),
-    ]
-    for state, method in states_and_methods:
-        obj = main()
-        obj.current_state = state
-        try:
-            getattr(obj, method)()
-        except StateMachineException as e:
-            assert str(e) == 'unsupported'
 
 
 def test_unsupported_due_to_unmet_conditions():
-    obj = main()
-    obj.current_state = 't7'
-    obj.variables['c'] = None
+    o = main()
+    o.current_state = 't0'
+    o.variables['x'] = 2
     try:
-        obj.cast()
-    except StateMachineException as e:
-        assert str(e) == 'unsupported'
-    obj.current_state = 't0'
-    obj.variables['x'] = 2
-    try:
-        obj.spin()
+        o.spin()
     except StateMachineException as e:
         assert str(e) == 'unsupported'
 
 
 def test_unsupported_in_non_existent_state():
-    obj = main()
-    obj.current_state = 'non_existent_state'
-    for method in ['visit', 'cast', 'spin', 'hike']:
+    o = main()
+    o.current_state = 'non_existent_state'
+    for m in ['visit', 'cast', 'spin', 'hike']:
         try:
-            getattr(obj, method)()
+            getattr(o, m)()
         except StateMachineException as e:
             assert str(e) == 'unsupported'
 
 
 def test_seen_method():
-    obj = main()
-    assert not obj.seen_method('visit')
-    obj.visit()
-    assert obj.seen_method('visit')
-    assert not obj.seen_method('cast')
+    o = main()
+    o.visit()
+    assert o.seen_method('visit')
 
 
 def test_has_max_out_edges():
-    obj = main()
-    obj.current_state = 't3'
-    assert not obj.has_max_out_edges()
-    obj.current_state = 't7'
-    assert obj.has_max_out_edges()
-    obj.current_state = 'nonexistent'
-    assert not obj.has_max_out_edges()
+    o = main()
+    o.current_state = 't3'
+    assert not o.has_max_out_edges()
+    o.current_state = 'nonexistent'
+    assert not o.has_max_out_edges()
 
 
 def test_has_path_to():
-    obj = main()
-    obj.current_state = 't3'
-    assert obj.has_path_to('t4')
-    assert obj.has_path_to('t7')
-    assert not obj.has_path_to('nonexistent')
-    obj.current_state = 't0'
-    assert obj.has_path_to('t4')
-    assert obj.has_path_to('t6')
-    obj.current_state = 't5'
-    assert obj.has_path_to('t7')
-    assert not obj.has_path_to('t3')
+    o = main()
+    o.current_state = 't3'
+    assert o.has_path_to('t4')
+    assert not o.has_path_to('nonexistent')
 
 
-def test_link_and_close():
-    obj = main()
+def test_getattr_multiple_unknown_methods():
+    o = main()
+    for method in ['race', 'chain', 'sort']:
+        try:
+            getattr(o, method)()
+        except StateMachineException as e:
+            assert str(e) == 'unknown'
+
+
+def test_swap_unsupported_state():
+    o = main()
+    o.current_state = 't3'
     try:
-        obj.link()
+        o.swap()
     except StateMachineException as e:
-        assert str(e) == 'unknown'
+        assert str(e) == 'unsupported'
+
+
+def test_cast_with_unmet_condition():
+    o = main()
+    o.current_state = 't7'
+    o.let_c(2)
     try:
-        obj.close()
+        o.cast()
     except StateMachineException as e:
-        assert str(e) == 'unknown'
+        assert str(e) == 'unsupported'
+
+
+def test_link_open_close_methods():
+    o = main()
+    for method in ['link', 'open', 'close']:
+        try:
+            getattr(o, method)()
+        except StateMachineException as e:
+            assert str(e) == 'unknown'
 
 
 def test():
@@ -370,11 +329,12 @@ def test():
     test_spin_method_with_conditions()
     test_hike_method()
     test_hike_method_unsupported()
-    test_hike_method_in_t5()
-    test_unsupported_method_in_current_state()
     test_unsupported_due_to_unmet_conditions()
     test_unsupported_in_non_existent_state()
     test_seen_method()
     test_has_max_out_edges()
     test_has_path_to()
-    test_link_and_close()
+    test_getattr_multiple_unknown_methods()
+    test_swap_unsupported_state()
+    test_cast_with_unmet_condition()
+    test_link_open_close_methods()
